7fc2ef0e6f88e225a4cd71f4d34c328d56fac07c
wanzbrayyy <wanzbray@gmail.com>
2025-11-03 16:55:39 +0700

    Add files via upload

--- routes/mainRoutes.js
+++ routes/mainRoutes.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const router = express.Router();
 const mainController = require('../controllers/mainController');
-
 router.get('/', mainController.getHomePage);
 router.post('/check-domain', mainController.checkDomain);
 router.get('/order-domain', mainController.orderDomain);
 router.get('/ssl', mainController.getSslPage);
+router.get('/check-ip', mainController.checkIp);
+
+router.get('/robots.txt', mainController.getRobotsTxt);
+router.get('/sitemap.xml', mainController.getSitemapXml);
+
 router.use((req, res, next) => {
     if (req.query.ref) {
-        res.cookie('ref_code', req.query.ref, { maxAge: 86400000, httpOnly: true }); 
+        res.cookie('ref_code', req.query.ref, { maxAge: 86400000, httpOnly: true });
     }
     next();
 });
