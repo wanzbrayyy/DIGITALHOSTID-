e33e08f020b0182e13545abec9822fef6ae9b809
wanzbrayyy <wanzbray@gmail.com>
2025-11-05 04:49:09 +0700

    Add files via upload

--- routes/checkoutRoutes.js
+++ routes/checkoutRoutes.js
@@ -4,12 +4,12 @@ const checkoutController = require('../controllers/checkoutController');
 const { ensureAuthenticated } = require('../middleware/auth');
 
 router.post('/add-to-cart', checkoutController.addToCart);
-
 router.get('/checkout', ensureAuthenticated, checkoutController.getCheckoutPage);
 router.post('/update-cart-options', ensureAuthenticated, checkoutController.updateCartOptions);
 router.post('/apply-voucher', ensureAuthenticated, checkoutController.applyVoucher);
 router.get('/remove-voucher', ensureAuthenticated, checkoutController.removeVoucher);
-
 router.post('/process-checkout', ensureAuthenticated, checkoutController.processCheckout);
+
+router.post('/update-cart-domain', ensureAuthenticated, checkoutController.updateCartDomain);
 
 module.exports = router;
